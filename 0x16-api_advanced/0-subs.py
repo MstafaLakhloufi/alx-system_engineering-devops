ae3750db1616fa758d2e3d1dc56e85e6584fb373
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2024-08-18 10:38:33 -0700

    m

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,32 +5,19 @@ Query a subreddit and return the number of
 total subscribers in that subredit
 """
 
-from requests import get
-from sys import argv
+import requests
 
-
-headers = {
-    "User-Agent": "Of course I had to use a custom User-Agent",
-    "X-Forwared-For": "iamthecavalry"
-}
-
-
-def number_of_subscribers(subreddit: str) -> int:
+def number_of_subscribers(subreddit):
     """
-    Query the subreddit and return the number of
-    Active subs. If its an invalid subredit, return 0
+    Function that queries the Reddit API
+    - If not a valid subreddit, return 0.
     """
-    response = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
-                   headers=headers)
-    data = response.json()
-    try:
-        if 'error' in data.keys():
-            return 0
-        else:
-            return data['data']['subscribers']
-    except Exception as e:
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
+
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
+    else:
         return 0
-
-
-if __name__ == "__main__":
-    print(number_of_subscribers(argv[1]))
