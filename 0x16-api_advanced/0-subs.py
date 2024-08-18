0f6a42be89e065b4492fb54d3533ccf8131b127e
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2024-08-18 10:48:18 -0700

    um

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 """
 Query a subreddit and return the number of
-total subscribers in that subredit
+total subscribers in that subreddit
 """
 import requests
 
@@ -17,4 +16,4 @@ def number_of_subscribers(subreddit):
     if response.status_code == 404:
         return 0
     results = response.json().get("data")
-    return results.get("subscribers")
+    return results.get("subscribers", 0)
