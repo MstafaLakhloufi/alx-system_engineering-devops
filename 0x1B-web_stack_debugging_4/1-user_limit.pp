9845f501162ba7d12c452f0e7cb45c17f34d2985
MstafaLakhloufi <mstafalakhloufi@gmail.com>
2024-08-24 09:14:02 -0700

    m

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+# fix limit file at holberton user.
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
